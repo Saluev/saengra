675ad78fd3fe1190669ba7c14716fe42ff8796ff
Tigran Saluev <tigran@saluev.com>
2026-03-09 00:22:50 +0400

    Fix formatting

--- saengra/utilities/properties.py
+++ saengra/utilities/properties.py
@@ -14,7 +14,6 @@ from saengra.utilities.annotations import (
 )
 from saengra.utilities.containers import RelatedEntitiesSet
 
-
 _object_getattribute = object.__getattribute__
 _object_setattr = object.__setattr__
 _object_delattr = object.__delattr__
@@ -30,7 +29,9 @@ class EntityProperty:
         """Returns the list of attribute names that are going to be used and have to be added to entity's __slots__."""
         return []
 
-    def reset_cache(self, env: "EnvProtocol", e: "Entity", primitive: Primitive) -> None:
+    def reset_cache(
+        self, env: "EnvProtocol", e: "Entity", primitive: Primitive
+    ) -> None:
         pass
 
     def clear_cache(self, e: "Entity") -> None:
@@ -101,7 +102,9 @@ class RelatedCollectionProperty(EntityProperty):
     def compose_slots(self) -> list[str]:
         return [self._cache_attr_name]
 
-    def reset_cache(self, env: "EnvProtocol", e: "Entity", primitive: Primitive) -> None:
+    def reset_cache(
+        self, env: "EnvProtocol", e: "Entity", primitive: Primitive
+    ) -> None:
         collection = self.make_collection(env, e, primitive)
         _object_setattr(e, self._cache_attr_name, collection)
 
@@ -157,7 +160,9 @@ class RelatedOptionalEntityProperty(RelatedEntityPropertyBase):
     def compose_init_code(self) -> tuple[list[str], dict[str, Any]]:
         return [], {}
 
-    def reset_cache(self, env: "EnvProtocol", e: "Entity", primitive: Primitive) -> None:
+    def reset_cache(
+        self, env: "EnvProtocol", e: "Entity", primitive: Primitive
+    ) -> None:
         _object_setattr(e, self._cache_attr_name, None)
 
     def clear_cache(self, e: "Entity") -> None:
@@ -216,7 +221,9 @@ class RelatedEntityProperty(RelatedEntityPropertyBase):
         result = [f"self.{self._cache_attr_name} = _uninitialized"]
         return result, {"_uninitialized": _uninitialized}
 
-    def reset_cache(self, env: "EnvProtocol", e: "Entity", primitive: Primitive) -> None:
+    def reset_cache(
+        self, env: "EnvProtocol", e: "Entity", primitive: Primitive
+    ) -> None:
         pass  # don't init cache, this attribute has to be set
 
     def clear_cache(self, e: "Entity") -> None:
